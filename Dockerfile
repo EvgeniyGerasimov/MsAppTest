45fb9089829186b9254e28248053f8e36e4902ca
Evgeniy-Gerasimov <evgeniy.gerasimov@motorsport.com>
2019-04-19 15:30:19 +0300

    add variables

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
+#install debian and python
 FROM python:3.7.2-slim
-
+# Add variables SERVER
+ENV SERVER $SERVER
+#get git
 RUN apt-get update -y
 RUN apt-get install git -y
+#clone project
 RUN git clone https://github.com/EvgeniyGerasimov/MsAppTest.git /project
+#go to project and load dependencies
 WORKDIR /project
 RUN pip3 install -r requirements.txt
 
-CMD ["pytest", "-n4"]
+#run tests on threads
+CMD ["pytest", "-n auto"]
--- data/methods.py
+++ data/methods.py
@@ -1,22 +1,28 @@
 import requests
+import os
 
 
 class Methods:
+    server = os.getenv('SERVER', "")
+    headers = {'Content-Type': 'application/json', 'location': 'ua'}
+    base_url = 'http://api' + server + '-v1.motorsport.com'
 
-    headers = {'Content-Type': 'application/json'}
-    base_url = 'http://api-s-v1.motorsport.com'
     @staticmethod
-    def get(url):
+    def get(url, headers,params=None):
         result = requests.get(Methods.base_url + url,
-                              headers=Methods.headers,
-                              params=None)
+                              headers=headers,
+                              params=params
+                              )
         return result
+
     @staticmethod
-    def post(url, body):
+    def post(url, body, headers):
         result = requests.post(Methods.base_url + url,
                                json=body,
-                               headers=Methods.headers,
+                               headers=headers,
+                               params=None
                                )
         return result
+
 
 
